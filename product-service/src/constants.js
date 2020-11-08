cd076b03fbdb3e55b8667e97f942e6d656156592
Aliaksei Hurynovich <shagon1k@gmail.com>
2020-11-08 15:09:43 +0300

    feat: added add product request validation on lambda level

--- product-service/src/constants.js
+++ product-service/src/constants.js
@@ -1,5 +1,6 @@
 export const ERROR_MESSAGES = {
   NO_SUCH_PRODUCT: 'No guitar with such id!',
   CONVERT_PRICE_ERROR: 'Currency convert error!',
-  DB_HANDLING_ERROR: 'Database handling error!'
+  DB_HANDLING_ERROR: 'Database handling error!',
+  INVALID_PRODUCT: 'Invalid product data passed. Required fields: title, description, imageurl, price'
 };
